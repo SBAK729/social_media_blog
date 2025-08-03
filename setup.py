118f366a6577c19579cd2874ed1557d6fadd48ac
SBAK729 <sench729@gmail.com>
2025-08-03 23:03:35 +0300

    feat: creat setup file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    namae="Social Media Blog",
+    version="0.0.0",
+    author="Sintayehu Bikila",
+    author_email="sench729@gmail.com",
+    packages= find_packages(),
+    insall_requires = []
+)
