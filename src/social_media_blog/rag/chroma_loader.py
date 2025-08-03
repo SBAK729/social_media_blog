fec7de6e1448303c61bca86e0c9bf8ca6c61e0ce
SBAK729 <sench729@gmail.com>
2025-08-03 23:08:03 +0300

    feat: complet vector search logic

---
+++ src/social_media_blog/rag/chroma_loader.py
@@ -0,0 +1,9 @@
+from langchain_community.vectorstores import Chroma
+from social_media_blog.utils.helper import download_hugging_face_embeddings
+
+def load_chroma_db(persist_directory: str = "chroma_db"):
+    embedding = download_hugging_face_embeddings()
+    vectordb = Chroma(persist_directory=persist_directory, embedding_function=embedding)
+    return vectordb
+
+# load_chroma_db()
